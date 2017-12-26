f55e7df9ddf4d1813b42484c56a6f6d2fdec8162
Jade Dominguez <plusjade@gmail.com>
2017-12-25 23:38:47 -0800

    Make banner more like the left-most dates.

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -2,45 +2,32 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Radium from 'radium'
 
-import stickybits from 'stickybits'
-
 const style = {
   default: {
-    position: "-webkit-sticky",
-    position: "-moz-sticky",
-    position: "-ms-sticky",
-    position: "-o-sticky",
-    position: "sticky",
-    top: 0,
-    left: 0,
-    right: 0,
     color: "#FFF",
-    padding: "0 10px",
-    height: 60,
-    lineHeight: "60px",
-    fontSize: 26,
+    margin: "10px 0",
+    padding: "0 15px",
+    lineHeight: "26px",
+    fontSize: 14,
     textAlign: "center",
     zIndex: 2,
-    backgroundColor: "#F5F5F5",
-    margin: "20px 0",
+    display: "inline-block",
+    borderRadius: "0 10px 10px 0",
   }
 }
 
 class Banner extends PureComponent {
-  componentDidMount() {
-    setTimeout(() => {
-      stickybits('.sticky')
-    }, 300)
-  }
   render() {
     return (
-      <div className="sticky" style={[
-        style.default,
-        {
-          color: this.props.color,
-        }
-      ]}>
-        {this.props.value}
+      <div
+        style={[
+          style.default,
+          {
+            backgroundColor: this.props.color,
+          }
+        ]}
+      >
+        {this.props.value.substring(0,3).toUpperCase()}
       </div>
     )
   }
